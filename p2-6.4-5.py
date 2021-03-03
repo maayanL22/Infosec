5c7f5d8aee3aff5ef72ee98c21ec337adcdfa1d9
maayanL22 <79762587+maayanL22@users.noreply.github.com>
2021-03-03 13:11:05 +0200

    Update and rename part2lesson6.4hackxercise5.txt to p2-6.4-5.py

--- p2-6.4-5.py
+++ p2-6.4-5.py
@@ -1,12 +1,9 @@
-unlocking information seecurity part II lesson 6.4 hackxercise 5:
-
 from scapy.all import *
 from scapy.layers.http import *
 import sys # ignore
 sys.path.insert(0,'.') # ignore
 from create_recording import recording_path # the path to the pcap file of this assignment
 
-#### Don't change the code until this line ####
 
 def show_source_destination_ports():
     packets = rdpcap(recording_path)
