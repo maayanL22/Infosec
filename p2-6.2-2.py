1e43e44fb7f2c039302b5ebd5407f327734395b1
maayanL22 <79762587+maayanL22@users.noreply.github.com>
2021-03-03 13:08:18 +0200

    Update and rename part2lesson6.2hackxercise2.txt to p2-6.2-2.py

--- p2-6.2-2.py
+++ p2-6.2-2.py
@@ -1,12 +1,9 @@
-unlocking information security part II lesson 6.2 hackxersice 2 final and correct code:
-
 from scapy.all import *
 from scapy.layers.http import *
 import sys # ignore
 sys.path.insert(0,'.') # ignore
 from create_recording import recording_path # the path to the pcap file of this assignment
 
-#### Don't change the code until this line ####
 
 def show_mac_address():
     packets = rdpcap(recording_path)
